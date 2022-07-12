d3bf6f0fb3baba8f65706a099c1718d0be29dc24
hemalathasrianusuri <104627794+hemalathasrianusuri@users.noreply.github.com>
2022-07-12 18:38:27 +0530

    Add files via upload

---
+++ website.js
@@ -0,0 +1,27 @@
+  function change()
+  {
+    document.getElementById("i").src="gcbvnbmb" ; 
+    no.style.diplay="block";
+  }
+  function unchange()
+  {
+     document.getElementById("i").src="vine.jpg"  ; 
+  }
+  function myfunction()
+  {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  function validate()
+  {
+    var r1=change();
+    var r2=unchange();
+    var r3=myfunction();
+    if(r1&&r2&&r3)
+     {
+       return true;
+     }
+     else
+     {
+       return false;
+     }
+  }
